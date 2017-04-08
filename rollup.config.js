5308100dcae0208c5f8735a396d4721f39148f3f
Jack Q <QiaoBo@outlook.com>
2017-04-08 20:47:41 +0800

    modify compile config

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,17 @@
-import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 
 export default {
   entry : 'src/webworker.js',
   format : 'iife',
   dest : "web/worker.js",
-  plugins : [resolve(), babel({
-      exclude: 'node_modules/**' // only transpile our source code
+  plugins : [babel({
+      "presets": [
+        [
+          "env", {
+            "modules": false,
+            "loose": true
+          }
+        ]
+      ]
     })]
 };
